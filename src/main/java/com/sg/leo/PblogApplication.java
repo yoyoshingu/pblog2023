d3eb6b91b98dd84b16e060c7a1959abfdbab5f94
Y Lee <leo@shingu.ac.kr>
2023-04-27 13:50:34 +0900

    첫번째커밋

---
+++ src/main/java/com/sg/leo/PblogApplication.java
@@ -0,0 +1,13 @@
+package com.sg.leo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PblogApplication.class, args);
+	}
+
+}
